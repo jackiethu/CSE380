b67bec9028fc98c48030912140aca6265afadb2e
Jiaqi Li <jiaqi@ices.utexas.edu>
2018-09-27 18:12:34 -0500

    Add a conditional to add.sh, examining the number of arguments

--- hw02/add.sh
+++ hw02/add.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Add two integers supplied as command line arguments
 
-echo "add.sh a b"
-echo $(($1 + $2))
+echo 'Usage: add.sh NUM1 NUM2'
+if [ $# -lt 2 ]; then
+    echo "The number of arguments is insufficient"
+else
+    echo $(($1 + $2))
+fi
