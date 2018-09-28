0d2efc48af7c17b5b382780f8bd9bbe8618239e3
Jiaqi Li <jiaqi@ices.utexas.edu>
2018-09-28 14:06:03 -0500

    Modify tri.sh: print numerical arrays

--- hw02/tri.sh
+++ hw02/tri.sh
@@ -2,5 +2,16 @@
 # Generate triangle patterns
 
 for i in {1..5}; do
-    echo $i
+    j=$(( 5 - i ))
+    while [ $j -ge 1 ]; do
+        echo -n " "
+        j=$(( j - 1 ))
+    done
+    printf "%b" "$i"
+    j=$(( i - 1 ))
+    while [ $j -ge 1 ]; do
+        echo -n " $i"
+        j=$(( j - 1 ))
+    done
+    printf "\n"
 done
