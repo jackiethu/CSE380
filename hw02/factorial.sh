157047a927bd5e1c35f498893b3a97753e8d676f
Jiaqi Li <jiaqi@ices.utexas.edu>
2018-09-27 22:11:43 -0500

    Modify factorial.sh: add argument detection

--- hw02/factorial.sh
+++ hw02/factorial.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 # Calcultate the factorial of a supplied number
 
-n=$1
-fac=1
-
-while [ $n -ge 2 ]; do
-    fac=$( expr $fac \* $n )
-    n=$( expr $n - 1 )
-done
-
-echo $fac
+echo "Usage: factorial.sh NUM"
+if [ -z "$1" ]; then
+    echo "No argument supplied"
+else
+    n=$1
+    fac=1
+    
+    while [ $n -ge 2 ]; do
+        fac=$( expr $fac \* $n )
+        n=$( expr $n - 1 )
+    done
+    
+    echo $fac
+fi
