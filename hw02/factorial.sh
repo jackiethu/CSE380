409cd1c0a1a26a165fc36341a842175d11bde845
Jiaqi Li <jiaqi@ices.utexas.edu>
2018-09-27 21:37:01 -0500

    Modify factorial.sh: use expr

--- hw02/factorial.sh
+++ hw02/factorial.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Calcultate the factorial of a supplied number
 
-n=$1;
-fac=1;
+n=$1
+fac=1
 
 while [ $n -ge 2 ]; do
-    fac=$(( fac*n ));
-    n=$(( n - 1 ));
+    fac=$( expr $fac \* $n )
+    n=$( expr $n - 1 )
 done
 
 echo $fac
