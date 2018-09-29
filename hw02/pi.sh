8d3f15d7247181101b95fea3bd33f3bd6d9df35d
Jiaqi Li <jiaqi@ices.utexas.edu>
2018-09-28 21:58:11 -0500

    Modify pi.sh: add sampling loop

--- hw02/pi.sh
+++ hw02/pi.sh
@@ -5,3 +5,20 @@
 RandomDraw() {
     echo "scale=15; $RANDOM/32767" | bc -l
 }
+
+# Monte Carlo sampling
+if [ -z "$1" ]; then
+    echo "No arguments supplied" 1>&2
+    exit 1
+fi
+N=$1
+N_i=0
+for i in $(seq $N); do
+    x=$(RandomDraw)
+    y=$(RandomDraw)
+    r=$( echo "$x*$x + $y*$y <= 1" | bc -l )
+    if [ $r -eq 1 ]; then 
+        N_i=$(( N_i + 1 ))
+    fi
+done
+echo $N $N_i $(echo "$N_i / $N" | bc -l)
