071ecfafed8feede718b1bb9af4828517636553c
Jiaqi Li <jiaqi@ices.utexas.edu>
2018-11-23 21:57:12 -0600

    Add subroutine Get_Matrix_2D_Order4 and verified

--- proj01/src/Get_Matrix_Mod.f90
+++ proj01/src/Get_Matrix_Mod.f90
@@ -51,7 +51,7 @@ contains
         integer :: n ! A is an n*n matrix
         
         n = A%n
-        if (n <= 4) stop "mesh number is too small to apply 4th order scheme"
+        if (n <= 4) stop "too few meshes to apply 4th order scheme"
         if (.not. allocated(A%row)) stop "A%row not allocated"
 
         ! first row & last row
@@ -90,10 +90,9 @@ contains
         integer :: n ! n is the number of grid points in each direction
 
         n = nint( sqrt( real(A%n) ) ) ! A is an n^2 * n^2 matrix
-
         if (.not. allocated(A%row)) stop "A%row not allocated"
 
-        ! first block
+        ! first block (each block consists of n rows)
         do i = 1, n
             indices_1 = i
             call A%row(i)%Init(1, indices_1, values_1)
@@ -131,6 +130,89 @@ contains
 
 
     subroutine Get_Matrix_2D_Order4
+        ! output: A for 2D problem with 4th order finite difference
+
+        use Solve_Common_Mod, only : A
+        implicit none
+        integer, dimension(1) :: indices_1 ! one-term index
+        integer, dimension(5) :: indices_5 ! five-term indices
+        integer, dimension(9) :: indices_9 ! nine-term indices
+        real, dimension(1) :: values_1 = [1.0]
+        real, dimension(5) :: values_5 = [1.0, 1.0, -4.0, 1.0, 1.0]
+        real, dimension(9) :: values_9 = [-1.0/12.0, 4.0/3.0, -1.0/12.0, &
+                      4.0/3.0, -5.0, 4.0/3.0, -1.0/12.0, 4.0/3.0, -1.0/12.0]
+
+        integer :: i, i_block
+        integer :: i_row ! temporary variable to store current row number
+        integer :: n ! n is the number of grid points in each direction
+
+        n = nint( sqrt( real(A%n) ) ) ! A is an n^2 * n^2 matrix
+        if (n <= 4) stop "too few meshes to apply 4th order scheme"
+        if (.not. allocated(A%row)) stop "A%row not allocated"
+
+        ! first block (each block consists of n rows)
+        do i = 1, n
+            indices_1 = i
+            call A%row(i)%Init(1, indices_1, values_1)
+        end do
+
+        ! last (nth) block
+        do i = n*n-n+1, n*n
+            indices_1 = i
+            call A%row(i)%Init(1, indices_1, values_1)
+        end do
+
+        ! 2nd block & (n-1)th block
+        do i_block = 2, n-1, n-3
+
+            ! first line in the block
+            i_row = n*(i_block-1) + 1
+            indices_1 = i_row
+            call A%row(i_row)%Init(1, indices_1, values_1)
+
+            ! last line in the block
+            i_row = n*(i_block-1) + n
+            indices_1 = i_row
+            call A%row(i_row)%Init(1, indices_1, values_1)
+
+            ! 2nd to (n-1)th line in the block
+            do i = 2, n-1
+                i_row = n*(i_block-1) + i
+                indices_5 = [i_row-n, i_row-1, i_row, i_row+1, i_row+n]
+                call A%row(i_row)%Init(5, indices_5, values_5)
+            end do
+
+        end do
+
+        ! 3rd to (n-2)th block
+        do i_block = 3, n-2
+
+            ! first line in the block
+            i_row = n*(i_block-1) + 1
+            indices_1 = i_row
+            call A%row(i_row)%Init(1, indices_1, values_1)
+
+            ! last line in the block
+            i_row = n*(i_block-1) + n
+            indices_1 = i_row
+            call A%row(i_row)%Init(1, indices_1, values_1)
+            
+            ! 2nd & (n-1)th line in the block
+            do i = 2, n-1, n-3
+                i_row = n*(i_block-1) + i
+                indices_5 = [i_row-n, i_row-1, i_row, i_row+1, i_row+n]
+                call A%row(i_row)%Init(5, indices_5, values_5)
+            end do
+
+            ! 3rd to (n-2)th line in the block
+            do i = 3, n-2
+                i_row = n*(i_block-1) + i
+                indices_9 = [i_row-2*n, i_row-n, i_row-2, i_row-1, i_row, &
+                                        i_row+1, i_row+2, i_row+n, i_row+2*n]
+                call A%row(i_row)%Init(9, indices_9, values_9)
+            end do
+
+        end do
 
     end subroutine Get_Matrix_2D_Order4
 
