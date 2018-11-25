7052693830b28dd59b37c8a13635224f8faacbd6
Jiaqi Li <jiaqi@ices.utexas.edu>
2018-11-25 13:08:12 -0600

    Add Solve_System_Mod

---
+++ proj01/src/Solve_System_Mod.f90
@@ -0,0 +1,58 @@
+! module to solve linear system Ax = b using iterative method
+module Solve_System_Mod
+    implicit none
+
+contains
+    
+    subroutine Solve_System
+        use Solve_Common_Mod, only : A, x, b
+        use Jacobi_GS_Mod, only : Update_Jacobi, Update_GS
+        use Control_Parameters_Mod, only : eps, max_iter, solver_Flag, &
+                                           debug_Flag
+        implicit none
+
+        integer :: i, n
+        real, allocatable, dimension(:) :: x_new
+        integer :: ierr ! error indicator
+        real :: err ! difference in x between two iterations
+
+        ! allocate x_new
+        n = size(x)
+        allocate(x_new(n), stat = ierr)
+        if (ierr /= 0) stop "unable to allocate x_new"
+
+        ! initialize x
+        x = 0
+
+        ! iteration
+        do i = 1, max_iter
+            select case(solver_Flag)
+            case(1)
+                x_new = Update_Jacobi(A, x, b)
+            case(2)
+                x_new = Update_GS(A, x, b)
+            case default
+                stop "invalid solver_Flag"
+            end select
+            err = norm2(x_new - x)
+            x = x_new
+            if (err <= eps) exit
+        end do
+
+        if (debug_Flag == 1) then
+            select case(solver_Flag)
+            case(1)
+                write(*, "('----- JACOBI METHOD -----')")
+            case(2)
+                write(*, "('----- GAUSS-SEIDEL METHOD -----')")
+            case default
+                stop "invalid solver_Flag"
+            end select
+            
+            write(*,"('iteration count: ', I6)") i
+            write(*,"('err = ', E13.6)") abs(err)
+        end if
+
+    end subroutine
+
+end module Solve_System_Mod
--- proj01/src/main.f90
+++ proj01/src/main.f90
@@ -2,16 +2,17 @@
 program main
     use Read_Input_Mod, only : Read_Input ! module to read input
     use Initialize_Mod, only : Initialize ! module to initialize matrix & vec
+    use Solve_System_Mod, only : Solve_System ! module to solve linear system
     implicit none
 
     ! open control file "input.dat" and read control parameters
     call Read_Input
 
-    ! initialize matrix and vectors A, x, b
+    ! initialize and build matrix and vectors A, x, b
     call Initialize
 
     ! solve the linear system Ax = b
-
+    call Solve_System
 
     ! postprocessing
 
