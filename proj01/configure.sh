3fdeb890e58535c06ccf401343aa762a278b2e70
Jiaqi Li <jiaqi@ices.utexas.edu>
2018-12-09 20:58:58 -0600

    Use --enable-coverage option now

--- proj01/configure.sh
+++ proj01/configure.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-module swap intel gnu7
-module load boost
+module load gnu7 boost
 export PKGPATH=/work/00161/karl/stampede2/public/
 ./configure FC=gfortran FCFLAGS='-g -O0' --with-masa=$PKGPATH/masa-gnu7-0.50 \
-    --with-grvy=$PKGPATH/grvy-gnu7-0.34 CODE_COVERAGE_ENABLED=1
+    --with-grvy=$PKGPATH/grvy-gnu7-0.34 --enable-coverage
